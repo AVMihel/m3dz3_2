1b4f7c0f0a4847c55fca1571f2cd049309a3ad83
AVMihel <dok2000@list.ru>
2025-02-07 10:22:56 +0500

    one

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        BmiService service = new BmiService();
+        double weight = 98;
+        double height = 1.87;
+        int bmi = service.calculate(weight, height);
+
+        System.out.println("Индекс массы тела:");
+        System.out.println(bmi);
+
+        System.out.println("Индекс массы тела:");
+        System.out.println(service.calculate(93, 1.85));
+
+        System.out.println("Индекс массы тела:");
+        System.out.println(service.calculate(85, 1.78));
     }
 }
